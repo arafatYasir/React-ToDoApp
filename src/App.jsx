2bb5095ab315b43a49c58acc7ddec673d23de227
arafatYasir <98328335+arafatYasir@users.noreply.github.com>
2025-01-01 17:34:19 +0600

    Task insertion functionality complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
+import { useEffect, useState } from "react";
+import Input from "./components/Input";
+
 function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAdd(newItem) {
+    const exists = items.find(item => item.text === newItem.text);
+    if (!exists) {
+      setItems([...items, newItem]);
+    }
+    console.log(items);
+  }
 
   return (
     <div>
+      <h3 className="inter text-center my-6 text-[32px] font-semibold">Todo List App with React.js</h3>
 
+      <div className="max-w-[500px] min-h-[350px] p-12 rounded-2xl bg-[#F5F5F5] shadow-2xl box-border mx-auto">
+        <Input handleAdd={handleAdd} />
+      </div>
     </div>
   )
 }
---
+++ src/components/Input.jsx
@@ -0,0 +1,23 @@
+import { useRef } from "react";
+
+const Input = ({ handleAdd }) => {
+    const inputBox = useRef();
+
+    function handleSubmit() {
+        const newItem = {
+            text: inputBox.current.value,
+            id: Date.now(),
+            completed: false
+        };
+        inputBox.current.value = "";
+        handleAdd(newItem);
+    }
+    return (
+        <div className="flex gap-[5px]">
+            <input ref={inputBox} className="border-0 rounded-2xl py-[10px] pl-[22px] w-[250px] bg-[#D9D9D9] text-[16px]" type="text" placeholder="New task..." />
+            <button onClick={handleSubmit} className="text-[16px] py-[10px] px-[22px] rounded-2xl border-0 bg-[#0074C7B2] cursor-pointer">Add</button>
+        </div>
+    );
+};
+
+export default Input;
